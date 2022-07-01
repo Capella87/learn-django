cbec26e8d0e04ac8010a78b66bb37c1d9ff0efae
Capella87 <contact@capelladev.com>
2022-07-01 23:37:57 +0900

    Migrate database to MariaDB

--- learn_django/settings.py
+++ learn_django/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'learn_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'admin',
+        'PASSWORD': 'naengmyeon!@#1',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET default_storage_engine=INNODB',
+        }
     }
 }
 
